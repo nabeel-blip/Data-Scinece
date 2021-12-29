9c61d8e1a3b3ab8179126fb4dff49519a0f11aa3
Nabeel <nabeelhassankhan17@gmail.com>
2021-12-29 11:23:42 +0500

    Reading data

---
+++ NetflixTitleAnalysis.R
@@ -0,0 +1,11 @@
+library(ggplot2)
+library(tidyverse)
+
+raw_data <- read.csv("C:\\Users\\Nabeel Hassan Khan\\Desktop\\Rstudio\\Data Scinece assginments\\netflix_titles.csv")
+colnames(raw_data)
+dim(raw_data)
+head(raw_data$rating)
+
+
+
+
