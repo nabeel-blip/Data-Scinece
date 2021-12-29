877320f68d5d9a7110cb9314a35da2e3d8165c79
Nabeel <nabeelhassankhan17@gmail.com>
2021-12-29 11:24:38 +0500

    Plotting Distribution of Content Ratings on Netflix

--- NetflixTitleAnalysis.R
+++ NetflixTitleAnalysis.R
@@ -7,5 +7,12 @@ dim(raw_data)
 head(raw_data$rating)
 
 
+#plot Distribution of Content Ratings on Netflix
+plot_data <-plyr::count(raw_data$rating)
+plot_data
+
+ggplot(plot_data, aes(x="",y = freq,fill = x)) +
+  geom_bar(width=1,stat = "identity",color= "white") +
+  coord_polar("y",start=0)
 
 
