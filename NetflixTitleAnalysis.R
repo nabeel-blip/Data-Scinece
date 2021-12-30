632edadf4a5da9599961b46a54d2d2e5fc00703e
Nabeel <nabeelhassankhan17@gmail.com>
2021-12-30 15:50:36 +0500

    Cleaning up code
    cleaned up code and updated plot legend of content production plot

--- NetflixTitleAnalysis.R
+++ NetflixTitleAnalysis.R
@@ -2,14 +2,9 @@ library(ggplot2)
 library(tidyverse)
 
 raw_data <- read.csv("C:\\Users\\Nabeel Hassan Khan\\Desktop\\Rstudio\\Data Scinece assginments\\netflix_titles.csv")
-colnames(raw_data)
-dim(raw_data)
-head(raw_data$rating)
-
 
 ##plot Distribution of Content Ratings on Netflix
 plot_data <-plyr::count(raw_data$rating)
-plot_data
 
 ggplot(plot_data, aes(x="",y = freq,fill = x)) +
   geom_bar(width=1,stat = "identity",color= "white") +
@@ -55,8 +50,10 @@ plot_data3<-raw_data %>%
     summarise(count =n()) 
 
 ggplot()+
-  geom_line(data = plot_data3 %>% filter(type=="Movie" & release_year >=2010 ),aes(release_year,count))+
-  geom_line(data = plot_data3 %>% filter(type=="TV Show" &  release_year >=2010),aes(release_year,count))
+  geom_line(data = plot_data3 %>% filter(type=="Movie" & release_year >=2010 ),aes(release_year,count,col = 'Movie'))+
+  geom_line(data = plot_data3 %>% filter(type=="TV Show" &  release_year >=2010),aes(release_year,count,col = 'TV Show'))
+ 
 
-head(raw_data$release_year)
+
+
  
