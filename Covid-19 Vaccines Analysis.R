c831237acabc8e679e238a8b8687f8e6cbf613b6
Nabeel <nabeelhassankhan17@gmail.com>
2022-01-07 10:51:39 +0500

    cleaning up code and finalizing plot

--- Covid-19 Vaccines Analysis.R
+++ Covid-19 Vaccines Analysis.R
@@ -1,7 +1,6 @@
 library(lubridate)
 library(plyr)
-
-
+library(tidyverse)
 
 #reading data
 raw_data<-read.csv("C:\\Users\\Nabeel Hassan Khan\\Desktop\\Rstudio\\Data Scinece assginments\\country_vaccinations.csv")
@@ -12,8 +11,20 @@ raw_data$date <- mdy_hms(raw_data$date)
 countries = c('England','Northern Ireland','Scotland','Wales')
 data <- raw_data %>%
   filter (!country %in% countries) 
-
 colnames(data)
-View(plyr::count(data$vaccines))
+df <- data[c("country","vaccines")]
+df <-(unique(df))
 
+some.eu.maps <- map_data("world", region = unique(data$country))
+
+region.lab.data <- some.eu.maps %>%
+  group_by(region) %>%
+  summarise(long = mean(long), lat = mean(lat))
+
+some.eu.maps["vaccine"] <- NA
+combined <- full_join(some.eu.maps, df, by = c("region"= "country"))   
+colnames(some.eu.maps)
+ggplot(combined, aes(x = long, y = lat)) +
+  geom_polygon(aes( group = group, fill = factor(vaccines)))+
+  theme(legend.position = "none")  
 
