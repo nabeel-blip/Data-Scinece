3f15cc3670c5162ff9469a91b14a700efec6c5af
Nabeel <nabeelhassankhan17@gmail.com>
2022-01-17 13:39:18 +0500

    Cleaning up code

--- Movie Rating Analysis.R
+++ Movie Rating Analysis.R
@@ -5,25 +5,19 @@ library(ggplot2)
 
 raw_data_movie <- read.csv("C:/Users/Nabeel Hassan Khan/Desktop/Rstudio/Data Scinece assginments/Movie Rating data/movies.dat", sep=':')
 raw_data_movie <-select(raw_data_movie,-X,-X.1)
-
-
 colnames(raw_data_movie ) <- c("ID","Title","Genre")
-
 raw_data_movie $ID <- as.numeric(raw_data_movie $ID)
 
-View(raw_data_movie )
 
 raw_data_ratings <- read.csv("C:/Users/Nabeel Hassan Khan/Desktop/Rstudio/Data Scinece assginments/Movie Rating data/ratings.dat", sep=':')
 raw_data_ratings <-select(raw_data_ratings,-X,-X.1,-X.2)
-
 colnames(raw_data_ratings ) <- c("User", "ID", "Ratings", "Timestamp")
-typeof(raw_data_ratings$ID)
+
+
 data <- left_join(raw_data_movie, raw_data_ratings, by = c("ID"= "ID"))   
 plot_data <-plyr::count(data$Ratings,vars =NULL)
 plot_data <-plot_data[-c(12),]
 
-dplyr::count(data, Ratings, sort = TRUE) 
-colnames(data)
 ggplot(plot_data, aes(x="", y=freq, fill=factor(x))) +
   geom_bar(stat="identity", width=1,col = "White") +
   coord_polar("y", start=0)+
