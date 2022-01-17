5d1765f39c1f5f86012aeb0f9bee2079d932251c
Nabeel <nabeelhassankhan17@gmail.com>
2022-01-17 20:42:07 +0500

    Sentiment Analysis

--- Twitter Sentiment Analysis.R
+++ Twitter Sentiment Analysis.R
@@ -6,3 +6,13 @@ text <- tolower(data$tweet)
 text <- gsub('[[:punct:]]', '',text)
 
 
+sentimentanalysis<-analyzeSentiment(text)
+newdata$Sentiment_Score<-sentimentanalysis$SentimentQDAP
+newdata$Sentiment<-convertToDirection(sentimentanalysis$SentimentQDAP)
+Positive= sum(newdata$Sentiment=='positive')
+Negative= sum(newdata$Sentiment=='negative')
+Neutral<-sum(newdata$Sentiment=="neutral")
+paste("Positive: ",Positive)
+paste("Negative: ",Negative)
+paste("Neutral:  ",Neutral)
+
